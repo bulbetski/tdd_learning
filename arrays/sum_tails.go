99d50b89ad50aa58f7c9256f8b3726786dd2c974
Zakhar <bulbetski42@gmail.com>
2021-03-05 21:12:28 +0300

    "sum_tails"

--- arrays/sum_tails.go
+++ arrays/sum_tails.go
@@ -1,1 +1,15 @@
-package integers
+package arrays
+
+func SumAllTails(arraysToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range arraysToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
